0edc12aa836806b348ed69daafb01b4b14ad8583
Martin Perren <martinperren@gmail.com>
2018-04-28 18:51:50 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var mapsLeft;
 var allowBan = false;
 
 var items = ['cache','dust2','inferno','mirage','nuke','overpass','train'];
-var PFitems = ['cache','dust2','inferno','mirage','nuke','overpass','train','subzero','dust2','canals'];
+
 
 
 client.on('ready', () => {
@@ -23,51 +23,28 @@ client.on("guildDelete", guild => {
 });
 
 
-client.on('message', message => {
-    if (message.content.toLowerCase() === '!mapveto' || message.content.toLowerCase() === '!map veto') {
-        message.reply('Enter !ActiveDutyVeto  OR  !PopflashVeto');
-        mapsLeft = 50;
-    }
-});
 
 
 client.on('message', message => {
     if (message.content.toLowerCase() === '!mapsleft') {
-        message.reply("Maps left: "+maps + " ("+(mapsLeft - 1)+")");
+        message.reply("Mapas disponibles: "+maps + " ("+(mapsLeft - 1)+")");
     }
 });
 
 
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!activedutyveto') {
+     mapsLeft = 50;
+    if (message.content.toLowerCase() === '!mapveto') {
         maps = 'dust2, cache, inferno, mirage, nuke, overpass, train';
-        message.reply('Active Duty Map Veto starting: Type !veto MapName to ban any of the following maps: ' + maps);
+        message.reply('Veto iniciado. Usa !veto (mapa) para banear uno de los siguientes: ' + maps);
         mapsLeft = maps.split(",").length;
         allowBan=true;
     }
 });
 
-client.on('message', message => {
-    if (message.content.toLowerCase() === '!popflashveto') {
-        maps = 'subzero, dust2, canals, cobble, cache, inferno, mirage, nuke, overpass, train';
-        message.reply('Popflash Map Veto starting: Type !veto MapName to ban any of the following maps: ' + maps);
-        mapsLeft = maps.split(",").length;
-        allowBan=true;
-    }
-});
 
-client.on('message', message => {
-    if (message.content.toLowerCase() === '!veto dust2' && maps.indexOf('dust2')!= -1 && allowBan) {
-        maps = maps.replace('dust2, ', '');
-        message.reply('Dust 2 removed. Maps left: ' + maps);
-        mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
-        {
-            message.reply("Map left: " + maps);
-        }
-    }
-});
+
 
 
 client.on('message', message => {
@@ -100,16 +77,28 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!veto dust2' && maps.indexOf('dust2')!= -1 && allowBan) {
+        maps = maps.replace('dust2, ', '');
+         message.reply('Dust 2 eliminado. Mapas disponibles: ' + maps);
+        mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Mapa: " + maps);
+        }
+    }
+});
+
 
 
 client.on('message', message => {
     if (message.content.toLowerCase() === '!veto cache' && maps.indexOf('cache')!= -1 && allowBan) {
         maps = maps.replace('cache, ', '');
         mapsLeft = maps.split(",").length;
-        message.reply('Cache removed. Maps left: ' + maps);
-        if(mapsLeft==2)
+        message.reply('Cache eliminado. Mapas disponibles: ' + maps);
+        if(mapsLeft==1)
         {
-            message.reply("Map left: " + maps);
+            message.reply("Mapa: " + maps);
         }
     }
 });
@@ -118,10 +107,10 @@ client.on('message', message => {
     if (message.content.toLowerCase() === '!veto mirage' && maps.indexOf('mirage')!= -1 && allowBan) {
         maps = maps.replace('mirage, ', '');
         mapsLeft = maps.split(",").length;
-        message.reply('Mirage removed. Maps left: ' + maps);
-        if(mapsLeft==2)
+         message.reply('Mirage eliminado. Mapas disponibles: ' + maps);
+        if(mapsLeft==1)
         {
-            message.reply("Map left: " + maps);
+            message.reply("Mapa: " + maps);
         }
     }
 });
@@ -130,11 +119,11 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content.toLowerCase() === '!veto nuke' && maps.indexOf('nuke')!= -1 && allowBan) {
         maps = maps.replace('nuke, ', '');
-        message.reply('Nuke removed. Maps left: ' + maps);
+         message.reply('Nuke eliminado. Mapas disponibles: ' + maps);
         mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
+        if(mapsLeft==1)
         {
-            message.reply("Map left: " + maps);
+            message.reply("Mapa: " + maps);
         }
     }
 });
@@ -144,11 +133,11 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content.toLowerCase() === '!veto overpass' && maps.indexOf('overpass')!= -1 && allowBan) {
         maps = maps.replace('overpass, ', '');
-        message.reply('Overpass removed. Maps left: ' + maps);
+         message.reply('Overpass eliminado. Mapas disponibles: ' + maps);
         mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
+        if(mapsLeft==1)
         {
-            message.reply("Map left: " + maps);
+            message.reply("Mapa: " + maps);
         }
     }
 });
@@ -157,11 +146,11 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content.toLowerCase() === '!veto train' && maps.indexOf('train')!= -1 && allowBan) {
         maps = maps.replace('train', '');
-        message.reply('Train removed. Maps left: ' + maps);
+         message.reply('Train eliminado. Mapas disponibles: ' + maps);
         mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
+        if(mapsLeft=1)
         {
-            message.reply("Map left: " + maps);
+            message.reply("Mapa: " + maps);
         }
     }
 });
@@ -170,64 +159,16 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content.toLowerCase() === '!veto inferno' && maps.indexOf('inferno')!= -1 && allowBan ) {
         maps = maps.replace('inferno, ', '');
-        message.reply('Inferno removed. Maps left: ' + maps);
+        message.reply('Inferno eliminado. Mapas disponibles: ' + maps);
         mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
+        if(mapsLeft==1)
         {
-            message.reply("Map left: " + maps);
+           message.reply("Mapa: " + maps);
         }
     }
 });
 
 
-client.on('message', message => {
-    if (message.content.toLowerCase() === '!veto canals' && maps.indexOf('canals')!= -1 &&allowBan) {
-        maps = maps.replace('canals, ', '');
-        message.reply('Canals removed. Maps left: ' + maps);
-        mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
-        {
-            message.reply("Map left: " + maps);
-        }
-    }
-});
-
-client.on('message', message => {
-    if (message.content.toLowerCase() === '!veto subzero' && maps.indexOf('subzero')!= -1 &&allowBan) {
-        maps = maps.replace('subzero, ', '');
-        message.reply('Subzero removed. Maps left: ' + maps);
-        mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
-        {
-            message.reply("Map left: " + maps);
-        }
-        
-    }
-});
-
-client.on('message', message => {
-    if (message.content.toLowerCase() === '!veto dust2' && maps.indexOf('dust2')!= -1 &&allowBan) {
-        maps = maps.replace('dust2, ', '');
-        message.reply('Dust 2 removed. Maps left: ' + maps);
-        mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
-        {
-            message.reply("Map left: " + maps);
-        }
-    }
-});
-client.on('message', message => {
-    if (message.content.toLowerCase() === '!veto dust 2' && maps.indexOf('dust2')!= -1 &&allowBan) {
-        maps = maps.replace('dust2, ', '');
-        message.reply('Dust 2 removed. Maps left: ' + maps);
-        mapsLeft = maps.split(",").length;
-        if(mapsLeft==2)
-        {
-            message.reply("Map left: " + maps);
-        }
-       
-    }
-});
 
 
 client.on('message',message=> {
@@ -237,12 +178,6 @@ client.on('message',message=> {
 });
 
 
-
-client.on('message',message=> {
-    if(message.content.toLowerCase() === '!randompopflashmap' || message.content.toLowerCase() === '!random popflash map' ) {
-     message.reply( PFitems[Math.floor(Math.random()*PFitems.length)] );
-    }
-});
 
 
 client.login(process.env.BOT_TOKEN);
